70281b2a304ab86d80307c277b7eecc68452b727
Soumya <dassoumyadip7407@gmail.com>
2024-05-11 12:39:17 +0530

    adding functionality for getPost & setLike

--- app/src/main/java/com/example/kinnectify/repository/Repository.kt
+++ app/src/main/java/com/example/kinnectify/repository/Repository.kt
@@ -9,13 +9,17 @@ import com.example.kinnectify.common.utils.Resource
 import com.example.kinnectify.models.Post
 import com.example.kinnectify.models.User
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.getValue
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.storage.StorageReference
 import javax.inject.Inject
 
 class Repository @Inject constructor(
-    private var refDatabase: DatabaseReference,
+    var refDatabase: DatabaseReference,
     private var refStorage: StorageReference,
     private var auth: FirebaseAuth,
     private var firebaseMessaging: FirebaseMessaging,
@@ -88,6 +92,72 @@ class Repository @Inject constructor(
         }
 
         return postLiveData
+    }
+
+    private val postsLiveData=MutableLiveData<Resource<List<Post>>>()
+    suspend   fun getPosts(): MutableLiveData<Resource<List<Post>>> {
+        postsLiveData.value = Resource.loading(null)
+
+        try {
+            var postList: ArrayList<Post> = ArrayList()
+            refDatabase.child(Constants.POSTS)
+                .addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        postList.clear()
+                        snapshot.children.forEach { child ->
+                            val post = child.getValue<Post>()
+                            postList.add(post!!)
+                        }
+                        postsLiveData.value = Resource.success(postList)
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        postsLiveData.value = Resource.error(error.message, null)
+
+                    }
+                })
+        }catch (e:Exception) {
+            postsLiveData.value = Resource.error(e.message.toString(), null)
+        }
+
+
+        return postsLiveData
+
+    }
+
+
+
+    suspend fun setLike(post: Post){
+
+
+        var postLikes: Int = post.postLikes
+        var mProcessLike = true
+        //get id of the post clicked
+        val postId: String = post.postId
+
+        if (postId.isNotEmpty()){
+            val myId= auth.currentUser?.uid
+            refDatabase.child(Constants.LIKES).addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if (mProcessLike) { //already liked ,so remove  like
+                        mProcessLike = if (dataSnapshot.child(postId).hasChild(myId!!)) {
+
+                            refDatabase.child(Constants.POSTS).child(postId).child(Constants.POSTLIKES).setValue(( -- postLikes))
+                            refDatabase.child(Constants.LIKES).child(postId).child(myId).removeValue()
+                            false
+                        } else { //not liked , liked it
+                            refDatabase.child(Constants.POSTS).child(postId).child(Constants.POSTLIKES).setValue(( ++ postLikes ))
+                            refDatabase.child(Constants.LIKES).child(postId).child(myId).setValue("Liked")
+                            false
+                        }
+                    }
+                }
+                override fun onCancelled(databaseError: DatabaseError) {
+                    Toast.makeText(context, "Error from likeRepo"+databaseError.message, Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+
     }
 
 
