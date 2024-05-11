5d02b88b95456b0d36c57a0985f2a2941c79d3ae
Soumya <dassoumyadip7407@gmail.com>
2024-05-11 12:45:47 +0530

    set up HomeFragment

--- app/src/main/java/com/example/kinnectify/ui/main/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/kinnectify/ui/main/fragment/HomeFragment.kt
@@ -1,60 +1,113 @@
 package com.example.kinnectify.ui.main.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kinnectify.R
+import com.example.kinnectify.ui.main.ViewModelMain
+import com.example.kinnectify.adapter.AdapterPost
+import com.example.kinnectify.databinding.FragmentHomeBinding
+import com.example.kinnectify.models.Post
+import com.example.kinnectify.common.utils.Status
+import com.google.firebase.auth.FirebaseAuth
+import com.ismaeldivita.chipnavigation.ChipNavigationBar
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+    @Inject
+    lateinit var auth: FirebaseAuth
+    @Inject
+    lateinit var homeAdapter: AdapterPost
+    private val viewModel by viewModels<ViewModelMain>()
+    private var postList: ArrayList<Post> = ArrayList()
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerViewSetUp()
+
+        viewModel.getPosts()
+        viewModel.postsLiveData.observe(viewLifecycleOwner) {
+            when (it.status) {
+                Status.LOADING -> {
+                    binding.homeProgressBar.visibility = View.VISIBLE
+                }
+                Status.SUCCESS -> {
+                    binding.homeProgressBar.visibility = View.GONE
+                    postList = it.data as ArrayList<Post>
+                    homeAdapter.setList(postList)
+                }
+                Status.ERROR -> {
+                    binding.homeProgressBar.visibility = View.GONE
+                    Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
                 }
             }
+        }
+
+        //setting up the OnClickListener for items in homeAdapter and sending post data through a bundle using putParcelable Method
+        homeAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putParcelable("post", it)
+            }
+            findNavController().navigate(R.id.action_homeFragment_to_postDetailsFragment, bundle)
+        }
+
+
+        homeAdapter.setOnItemClickListenerForGoingToOwner {
+            val bundle = Bundle().apply {
+                putParcelable("post", it)
+            }
+            if (it.userId == auth.currentUser?.uid ){
+                activity?.findViewById<ChipNavigationBar>(R.id.bottom_menu)?.setItemSelected(
+                    R.id.profile)
+
+                findNavController().navigate(R.id.profileFragment)
+
+            }else{
+                findNavController().navigate(R.id.action_homeFragment_to_postOwnerFragment, bundle)
+            }
+
+        }
+        homeAdapter.setonItemClickListenerForLike {
+            viewModel.setLike(it)
+        }
+
+
+
     }
+
+    private fun recyclerViewSetUp() {
+        val linearLayout = LinearLayoutManager(activity)
+        linearLayout.stackFromEnd = true
+        linearLayout.reverseLayout = true
+        binding.homeRv.layoutManager = linearLayout
+        homeAdapter.setList(postList)
+        binding.homeRv.adapter = homeAdapter
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
 }
